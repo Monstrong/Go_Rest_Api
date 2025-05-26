e9cfc19e969446de5bacbbee14ffda83b0561056
Monstrong <igrachev2006@gmail.com>
2025-05-26 17:17:28 +0300

    base structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Monstrong/Go_Rest_Api
+
+go 1.23.4
---
+++ internal/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+type Config struct {
+	Env string `yaml:"env" env:"ENV" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env:"STORAGE_PATH" env-default:".storage/storage.db"`
+	HTTPServer struct {
+		Address string `yaml:"address" env:"HTTP_SERVER_ADDRESS" env-default:"localhost:8082"`
+		Timeout string `yaml:"timeout" env:"HTTP_SERVER_TIMEOUT" env-default:"4s"`
+		IdleTimeout string `yaml:"idle_timeout" env:"HTTP_SERVER_IDLE_TIMEOUT" env-default:"60s"`
+	}
+}
