2e417d3ea03ee916cdf80081e3f50fc2da3f8120
Monstrong <igrachev2006@gmail.com>
2025-05-29 12:59:30 +0300

    config and logger inited

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,9 +1,53 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Monstrong/Go_Rest_Api/internal/config"
+)
+
+const (
+	envLocal  = "local"
+	envDev    = "dev"
+	envProd   = "prod"
+)
+
 func main() {
-	// TODO: init config: cleanenv
+	// init config: cleanenv
+	cfg := config.MustLoad()
+
 	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+
+	// log = log.With(slog.String("env", cfg.Env)) - тогда бы в каждое сообщение добавлялась инфа об окружении (у нас только в первой)
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
 	// TODO: init storage: sqlite
 	// TODO: init router: chi, render
 	// TODO: run server
+}
+
+// setupLogger creates a logger depending on the given environment.
+// It uses:
+// - slog.TextHandler with LevelDebug for local development
+// - slog.JSONHandler with LevelDebug for dev and prod environments
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,40 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type Config struct {
-	Env string `yaml:"env" env:"ENV" env-default:"local"`
-	StoragePath string `yaml:"storage_path" env:"STORAGE_PATH" env-default:".storage/storage.db"`
-	HTTPServer struct {
-		Address string `yaml:"address" env:"HTTP_SERVER_ADDRESS" env-default:"localhost:8082"`
-		Timeout string `yaml:"timeout" env:"HTTP_SERVER_TIMEOUT" env-default:"4s"`
-		IdleTimeout string `yaml:"idle_timeout" env:"HTTP_SERVER_IDLE_TIMEOUT" env-default:"60s"`
-	} 
+	Env         string     `yaml:"env" env:"ENV" env-default:"local"`
+	StoragePath string     `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
+	HTTPServer  HTTPServer `yaml:"http_server"`
+}
+
+// localhost по дефолту 8080, когда в yaml указан другой порт (8082)
+type HTTPServer struct {
+	Address     string `yaml:"address" env:"HTTP_SERVER_ADDRESS" env-default:"localhost:8080"`
+	Timeout     string `yaml:"timeout" env:"HTTP_SERVER_TIMEOUT" env-default:"4s"`
+	IdleTimeout string `yaml:"idle_timeout" env:"HTTP_SERVER_IDLE_TIMEOUT" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	//check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file %s does not exist", configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
 }
